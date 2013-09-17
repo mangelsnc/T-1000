b0f10de5d65820cbf559214ad88106f67252f821
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-09-18 01:34:11 +0200

    Implemente the class
    Implemented the DistanceRouter class

--- src/T1000/RoutePattern/DistanceRouter.php
+++ src/T1000/RoutePattern/DistanceRouter.php
@@ -3,10 +3,48 @@ namespace T1000\RoutePattern;
 
 require  __DIR__ . "/../../../autoloader.php";
 
-class DistanceRouter implements RoutePattern
+use T1000\RoutePattern\RoutePatternInterface;
+use T1000\RoutePattern\NoTargetException;
+
+class DistanceRouter implements RoutePatternInterface
 {
-    public function getNextTarget($targets)
+    const MAX_DISTANCE = 10000;
+
+    public function getNextTarget($origin, $targets)
     {
-        return $targets[0];
+        if(count($targets) == 0){
+            throw new NoTargetException();
+        }
+
+        $max = self::MAX_DISTANCE;
+        $next = null;
+
+        foreach($targets as $target){
+            $distance = $this->getDistance($origin, $target->getPosition());
+            
+            if($distance < $max){
+                $max = $distance;
+                $next = $target;
+            }
+        }
+
+        return $next;
     }
+
+    public function getDistance($origin, $target)
+    {
+        $x1 = $origin[0];
+        $y1 = $origin[1];
+
+        $x2 = $target[0];
+        $y2 = $target[1];
+
+        $primerParcial = pow(($x2 - $x1), 2);
+        $segundoParcial = pow(($y2 - $y1), 2);
+
+        $resultado = round(sqrt($primerParcial + $segundoParcial),2);
+
+        return $resultado;
+    }
+
 }
