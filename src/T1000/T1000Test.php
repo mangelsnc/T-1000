0eebf217233dd0f7fd93184c1eee3a8c771b3ee0
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-09-18 01:27:58 +0200

    Refactorized test
    Implemented a setUp method for initialize common objects used in tests.

--- src/T1000/T1000Test.php
+++ src/T1000/T1000Test.php
@@ -4,133 +4,91 @@ use T1000\T1000;
 
 class T1000Test extends PHPUnit_Framework_TestCase
 {
-    
+    private $firstTarget;
+    private $secondTarget;
+    private $routePattern;
+
+    public function setUp()
+    {
+        $this->firstTarget = $this->getMock('T1000\Target', array('getPosition'));
+        $this->firstTarget->expects($this->any())
+               ->method('getPosition')
+               ->will($this->returnValue(array(20, 30)));
+
+        $this->secondTarget = $this->getMock('T1000\Target', array('getPosition'));
+        $this->secondTarget->expects($this->any())
+               ->method('getPosition')
+               ->will($this->returnValue(array(50, 60)));
+
+        $this->routePattern = $this->getMock('T1000\RoutePattern', array('getNextTarget'));       
+        $this->routePattern->expects($this->any())
+                     ->method('getNextTarget')
+                     ->with($this->isType('array'))
+                     ->will($this->onConsecutiveCalls($this->firstTarget, $this->secondTarget));
+
+    }
+
     public function testT1000ShouldRequestForTarget()
     {
         $t1000 = new T1000();
-        $target = $this->getMock('T1000\Target', array('getPosition'));
-        $routePattern = $this->getMock('T1000\RoutePattern', array('getNextTarget'));
-        $routePattern->expects($this->once())
-                     ->method('getNextTarget')
-                     ->with($this->anything())
-                     ->will($this->returnValue($target));
         
-        $t1000->setRoutePattern($routePattern);
-        $t1000->addTarget($target);
+        $t1000->setRoutePattern($this->routePattern);
+        $t1000->addTarget($this->firstTarget);
         $t1000->moveToTarget();
     }
 
     public function testT1000ShouldMoveToTarget()
     {
         $t1000 = new T1000();
-        
-        $target = $this->getMock('T1000\Target', array('getPosition'));
-        $target->expects($this->any())
-               ->method('getPosition')
-               ->will($this->returnValue(array(20, 30)));
-        
-        $routePattern = $this->getMock('T1000\RoutePattern', array('getNextTarget'));
-        $routePattern->expects($this->once())
-                     ->method('getNextTarget')
-                     ->with($this->anything())
-                     ->will($this->returnValue($target));
 
-        $t1000->setRoutePattern($routePattern);
-        $t1000->addTarget($target);
+        $t1000->setRoutePattern($this->routePattern);
+        $t1000->addTarget($this->firstTarget);
         $t1000->moveToTarget();
 
-        $this->assertEquals($t1000->getPosition(), $target->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
     }
 
     public function testT1000AttendsChronologically()
     {
         $t1000 = new T1000();
-        
-        $firstTarget = $this->getMock('T1000\Target', array('getPosition'));
-        $firstTarget->expects($this->any())
-               ->method('getPosition')
-               ->will($this->returnValue(array(20, 30)));
 
-        $secondTarget = $this->getMock('T1000\Target', array('getPosition'));
-        $secondTarget->expects($this->any())
-               ->method('getPosition')
-               ->will($this->returnValue(array(50, 60)));
-        
-        $routePattern = $this->getMock('T1000\RoutePattern', array('getNextTarget'));
-        $routePattern->expects($this->any())
-                     ->method('getNextTarget')
-                     ->with($this->isType('array'))
-                     ->will($this->onConsecutiveCalls($firstTarget, $secondTarget));
-
-        $t1000->setRoutePattern($routePattern);
-        $t1000->addTarget($firstTarget);
-        $t1000->addTarget($secondTarget);
+        $t1000->setRoutePattern($this->routePattern);
+        $t1000->addTarget($this->firstTarget);
+        $t1000->addTarget($this->secondTarget);
         
         $t1000->moveToTarget();
-        $this->assertEquals($t1000->getPosition(), $firstTarget->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
         $t1000->moveToTarget();
-        $this->assertEquals($t1000->getPosition(), $secondTarget->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->secondTarget->getPosition());
     }
 
     public function testT1000GetsNextTargetWhenLostFirst()
     {
         $t1000 = new T1000();
-
-        $firstTarget = $this->getMock('T1000\Target', array('getPosition'));
-        $firstTarget->expects($this->any())
-            ->method('getPosition')
-            ->will($this->returnValue(array(20,30)));
-
-        $secondTarget = $this->getMock('T1000\Target', array('getPosition'));
-        $secondTarget->expects($this->any())
-            ->method('getPosition')
-            ->will($this->returnValue(array(50,60)));
-
-        $routePattern = $this->getMock('T1000\RoutePattern', array('getNextTarget'));
-        $routePattern->expects($this->any())
-                     ->method('getNextTarget')
-                     ->with($this->isType('array'))
-                     ->will($this->onConsecutiveCalls($firstTarget, $secondTarget));
         
-        $t1000->setRoutePattern($routePattern);
-        $t1000->addTarget($firstTarget);
-        $t1000->addTarget($secondTarget);
+        $t1000->setRoutePattern($this->routePattern);
+        $t1000->addTarget($this->firstTarget);
+        $t1000->addTarget($this->secondTarget);
 
         $t1000->moveToTarget();
-        $this->assertEquals($t1000->getPosition(), $firstTarget->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
         $t1000->targetLost();
-        $this->assertEquals($t1000->getPosition(), $secondTarget->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->secondTarget->getPosition());
 
     }
 
     public function testT1000AttendsNearestTargetFirst()
     {
         $t1000 = new T1000();
-
-        $firstTarget = $this->getMock('T1000\Target', array('getPosition'));
-        $firstTarget->expects($this->any())
-            ->method('getPosition')
-            ->will($this->returnValue(array(20,30)));
-
-        $secondTarget = $this->getMock('T1000\Target', array('getPosition'));
-        $secondTarget->expects($this->any())
-            ->method('getPosition')
-            ->will($this->returnValue(array(50,60)));
-
-        $routePattern = $this->getMock('T1000\RoutePattern', array('getNextTarget'));
-        $routePattern->expects($this->any())
-                     ->method('getNextTarget')
-                     ->with($this->isType('array'))
-                     ->will($this->onConsecutiveCalls($firstTarget, $secondTarget));
         
-        $t1000->setRoutePattern($routePattern);
-        $t1000->addTarget($firstTarget);
-        $t1000->addTarget($secondTarget);
+        $t1000->setRoutePattern($this->routePattern);
+        $t1000->addTarget($this->firstTarget);
+        $t1000->addTarget($this->secondTarget);
 
         $t1000->moveToTarget();
-        $this->assertEquals($t1000->getPosition(), $firstTarget->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
         $t1000->moveToTarget();
-        $this->assertEquals($t1000->getPosition(), $secondTarget->getPosition());
+        $this->assertEquals($t1000->getPosition(), $this->secondTarget->getPosition());
     }
     
 }
