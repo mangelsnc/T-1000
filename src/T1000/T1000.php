066038c0fe4c9db600fa0eb9575cd463bc0bb67b
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-09-20 01:00:23 +0200

    Added two method listeners and a event configurator
    Refactored the targetLost method to "onTargetLostEvent" to response
    when that event is triggered.
    
    Implemented the "onNewTargetEvent" to add a new target when that event
    is triggered.
    
    Implemente a method to configure the dispatcher registering  all the
    events

--- src/T1000/T1000.php
+++ src/T1000/T1000.php
@@ -3,6 +3,9 @@ namespace T1000;
 
 require_once "../../autoloader.php";
 
+use Symfony\Component\EventDispatcher\EventDispatcher as EventDispatcher;
+use T1000\T1000Events as T1000Events;
+
 class T1000
 {
     private $targets;
@@ -37,8 +40,19 @@ class T1000
         $this->routePattern = $routePattern;
     }
 
-    public function targetLost()
+    public function onTargetLostEvent()
     {
         $this->moveToTarget();
+    }
+
+    public function onNewTargetEvent($event)
+    {
+        $this->addTarget($event->getTarget());
+    }
+
+    public function registerEvents(EventDispatcher $dispatcher)
+    {
+        $dispatcher->addListener(T1000Events::NEW_TARGET, array($this, "onNewTargetEvent"));
+        $dispatcher->addListener(T1000Events::TARGET_LOST, array($this, "onTargetLostEvent"));
     }
 }
