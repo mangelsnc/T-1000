590d88e353212cb26f3e7c564bcf774982cd2e88
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-09-20 00:56:47 +0200

    Refactored whole test suite
    Refactored whole test suite to use the new Event Dispatcher system for
    the T1000 interaction

--- src/T1000/T1000Test.php
+++ src/T1000/T1000Test.php
@@ -1,12 +1,18 @@
 <?php
 require_once '../../autoloader.php';
+require_once '../../vendor/autoload.php';
+
 use T1000\T1000;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use T1000\T1000Events;
+use T1000\NewTargetEvent as NewTargetEvent;
 
 class T1000Test extends PHPUnit_Framework_TestCase
 {
     private $firstTarget;
     private $secondTarget;
     private $routePattern;
+    private $dispatcher;
 
     public function setUp()
     {
@@ -26,23 +32,29 @@ class T1000Test extends PHPUnit_Framework_TestCase
                      ->with($this->isType('array'))
                      ->will($this->onConsecutiveCalls($this->firstTarget, $this->secondTarget));
 
+        $this->dispatcher = new EventDispatcher();
+
     }
 
     public function testT1000ShouldRequestForTarget()
     {
         $t1000 = new T1000();
-        
+        $t1000->registerEvents($this->dispatcher);
         $t1000->setRoutePattern($this->routePattern);
-        $t1000->addTarget($this->firstTarget);
+        
+        $event = new NewTargetEvent($this->firstTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
         $t1000->moveToTarget();
     }
 
     public function testT1000ShouldMoveToTarget()
     {
         $t1000 = new T1000();
-
+        $t1000->registerEvents($this->dispatcher);
         $t1000->setRoutePattern($this->routePattern);
-        $t1000->addTarget($this->firstTarget);
+        
+        $event = new NewTargetEvent($this->firstTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
         $t1000->moveToTarget();
 
         $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
@@ -51,10 +63,13 @@ class T1000Test extends PHPUnit_Framework_TestCase
     public function testT1000AttendsChronologically()
     {
         $t1000 = new T1000();
-
+        $t1000->registerEvents($this->dispatcher);
         $t1000->setRoutePattern($this->routePattern);
-        $t1000->addTarget($this->firstTarget);
-        $t1000->addTarget($this->secondTarget);
+        
+        $event = new NewTargetEvent($this->firstTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
+        $event = new NewTargetEvent($this->secondTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
         
         $t1000->moveToTarget();
         $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
@@ -65,26 +80,46 @@ class T1000Test extends PHPUnit_Framework_TestCase
     public function testT1000GetsNextTargetWhenLostFirst()
     {
         $t1000 = new T1000();
-        
+        $t1000->registerEvents($this->dispatcher);
         $t1000->setRoutePattern($this->routePattern);
-        $t1000->addTarget($this->firstTarget);
-        $t1000->addTarget($this->secondTarget);
-
+        
+        $event = new NewTargetEvent($this->firstTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
+        $event = new NewTargetEvent($this->secondTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
         $t1000->moveToTarget();
+        
         $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
-        $t1000->targetLost();
+        $this->dispatcher->dispatch(T1000Events::TARGET_LOST);
         $this->assertEquals($t1000->getPosition(), $this->secondTarget->getPosition());
+    }
 
+    public function testT1000RecalculateTargetListWhenNewTargetIsAdded()
+    {
+        $t1000 = new T1000();
+        $t1000->registerEvents($this->dispatcher);
+        $t1000->setRoutePattern($this->routePattern);
+        
+        $event = new NewTargetEvent($this->firstTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
+        $event = new NewTargetEvent($this->secondTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
+        $t1000->moveToTarget();
+
+        $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());   
     }
 
     public function testT1000AttendsNearestTargetFirst()
     {
         $t1000 = new T1000();
-        
+        $t1000->registerEvents($this->dispatcher);
         $t1000->setRoutePattern($this->routePattern);
-        $t1000->addTarget($this->firstTarget);
-        $t1000->addTarget($this->secondTarget);
-
+        
+        $event = new NewTargetEvent($this->firstTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
+        $event = new NewTargetEvent($this->secondTarget);
+        $this->dispatcher->dispatch(T1000Events::NEW_TARGET, $event);
+        
         $t1000->moveToTarget();
         $this->assertEquals($t1000->getPosition(), $this->firstTarget->getPosition());
         $t1000->moveToTarget();
