79ec7d11c6d3196852edde7f01906dd5460c41d5
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-09-20 00:53:28 +0200

    Created a container class for the T1000 Events

---
+++ src/T1000/T1000Events.php
@@ -0,0 +1,15 @@
+<?php
+namespace T1000;
+
+final class T1000Events
+{
+    /**
+     *
+     * The only purpose of this class is to store
+     * all the event definitions for the T1000
+     *
+     */
+    
+    const TARGET_LOST = "t1000.target_lost";
+    const NEW_TARGET = "t1000.new_target";
+}
